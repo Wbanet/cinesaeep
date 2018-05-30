5db87fdb37d5ef887d731ad4d615be1413abda27
AntoMCpro <39759104+AntoMCpro@users.noreply.github.com>
2018-05-30 10:09:04 +0200

    gestionEntrada t GestionPelicula

--- src/negocio/GestionEntrada.java
+++ src/negocio/GestionEntrada.java
@@ -1,5 +1,31 @@
 package negocio;
 
+import java.util.List;
+
+import dto.Pelicula;
+
 public class GestionEntrada {
 
+	
+	public Pelicula comprobarTitulo(String titulo){
+		
+		return null;
+		
+	}
+	
+	public void añadirPelicula(String titulo){
+		
+	}
+	
+	public List<Pelicula> getPeliculas(List<Pelicula> peliculas){
+		return null;
+	}
+	
+	public void eliminarPelicula(Pelicula pelicula){
+		
+		
+	
+		
+	}
+	
 }
