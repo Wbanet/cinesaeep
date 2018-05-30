d19795e24f670154ebdae7172811d104c93bdaa5
Pedro <pedrogarciasuanzes@gmail.com>
2018-05-30 09:58:33 +0200

    Creado los atributos del dto

--- src/dto/Sala.java
+++ src/dto/Sala.java
@@ -2,4 +2,25 @@ package dto;
 
 public class Sala {
 
+	public int getNo_sala() {
+		return no_sala;
+	}
+	public void setNo_sala(int no_sala) {
+		this.no_sala = no_sala;
+	}
+	public int getFilas() {
+		return filas;
+	}
+	public void setFilas(int filas) {
+		this.filas = filas;
+	}
+	public int getAsientos_filas() {
+		return asientos_filas;
+	}
+	public void setAsientos_filas(int asientos_filas) {
+		this.asientos_filas = asientos_filas;
+	}
+	int no_sala;
+	int filas;
+	int asientos_filas;
 }
