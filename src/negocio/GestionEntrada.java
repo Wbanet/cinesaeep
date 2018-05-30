07fa43576e14afe59437ebe3a78c517e364bfb23
Oscar Arranz <theikkiscout@gmail.com>
2018-05-30 09:57:37 +0200

    Creados metodos GestionEntrada

--- src/negocio/GestionEntrada.java
+++ src/negocio/GestionEntrada.java
@@ -1,5 +1,34 @@
 package negocio;
 
+import dto.Pelicula;
+import dto.Sesion;
+
 public class GestionEntrada {
+	
+	// Necesito un escaner aqui
+	
+	public void mostrarMenu() {
+		
+	}
+	
+	public void buscarSesionPorPelicula() {
+		
+	}
+	
+	public void mostrarSesionesPorPelicula(Pelicula pelicula) {
+		
+	}
+	
+	public void mostrarGraficoSala(Sesion sesion) {
+		
+	}
+	
+	public void comprar(Sesion sesion) {
+		
+	}
+	
+	public void mostrarSesionesPorFecha() {
+		
+	}
 
 }
