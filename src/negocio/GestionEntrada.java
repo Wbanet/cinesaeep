0def62628d98a89f184a885a819e86a2e1f9f1d8
Oscar Arranz <theikkiscout@gmail.com>
2018-05-30 10:08:02 +0200

    Errores arreglados

--- src/negocio/GestionEntrada.java
+++ src/negocio/GestionEntrada.java
@@ -5,31 +5,5 @@ import dto.Sesion;
 import vista.utilidades.Escaner;
 
 public class GestionEntrada {
-	
-	Escaner escaner = new Escaner();
-	
-	public void mostrarMenu() {
-		
-	}
-	
-	public void buscarSesionPorPelicula() {
-		
-	}
-	
-	public void mostrarSesionesPorPelicula(Pelicula pelicula) {
-		
-	}
-	
-	public void mostrarGraficoSala(Sesion sesion) {
-		
-	}
-	
-	public void comprar(Sesion sesion) {
-		
-	}
-	
-	public void mostrarSesionesPorFecha() {
-		
-	}
 
 }
