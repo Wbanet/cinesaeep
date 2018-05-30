2fb703ca7a2873218b0813f4110c0c6813644731
AntoMCpro <39759104+AntoMCpro@users.noreply.github.com>
2018-05-30 10:10:28 +0200

    GestionEntrada

--- src/negocio/GestionEntrada.java
+++ src/negocio/GestionEntrada.java
@@ -1,37 +1,19 @@
 package negocio;
-
-<<<<<<< HEAD
 import java.util.List;
 
-import dto.Pelicula;
-=======
-import dto.Pelicula;
-import dto.Sesion;
-import vista.utilidades.Escaner;
->>>>>>> 0def62628d98a89f184a885a819e86a2e1f9f1d8
+import dao.EntradaDAO;
+import dto.Entrada;
 
 public class GestionEntrada {
 
+	EntradaDAO entradaDAO;
 	
-	public Pelicula comprobarTitulo(String titulo){
-		
-		return null;
-		
+	public boolean comprobarEntrada(Entrada entrada){
+		return true;
 	}
 	
-	public void añadirPelicula(String titulo){
-		
-	}
 	
-	public List<Pelicula> getPeliculas(List<Pelicula> peliculas){
-		return null;
-	}
 	
-	public void eliminarPelicula(Pelicula pelicula){
-		
-		
 	
-		
-	}
 	
 }
