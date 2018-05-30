7dc5fa780471d1639060ffeb959ca16f16c1598e
Jacquie327 <39758355+Jacquie327@users.noreply.github.com>
2018-05-30 10:13:29 +0200

    creados metodos vacios de DAO

--- src/dao/EntradaDAO.java
+++ src/dao/EntradaDAO.java
@@ -1,5 +1,19 @@
 package dao;
 
+import java.sql.Connection;
+
+import dto.Entrada;
+
 public class EntradaDAO {
 
+	Connection Conexion;
+	
+	public boolean comprobarEntrada (Entrada entrada) {
+		
+		return true;
+		}
+	
+	public void guardarEntrada (Entrada entrada) {
+		
+	}
 }
--- src/dao/PeliculaDAO.java
+++ src/dao/PeliculaDAO.java
@@ -1,5 +1,29 @@
 package dao;
 
+import java.sql.Connection;
+import java.util.List;
+
+import dto.Pelicula;
+
 public class PeliculaDAO {
 
+		Connection conexion;
+	
+	
+	public Pelicula comprobarTitulo (String titulo) {
+		return null;
+	}
+	
+	public void añadirPelicula (String titulo) {
+		
+	}
+	
+	public List<Pelicula> getPeliculas (){
+		return null;
+	}
+	
+	public void eliminarPelicula (Pelicula pelicula) {
+		
+	}
+	
 }
